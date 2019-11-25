73aee825a54bbc3d770dcfc077bb3ddd87cdcc05
deiry <sofia.navas96@gmail.com>
2019-11-25 12:57:50 -0500

    Clase intro

--- Labs/install-lib.sh
+++ Labs/install-lib.sh
@@ -1,3 +1,9 @@
-pip3 install httplib2
-pip3 install numpy
-pip3 install pixiedust
+pip install httplib2
+pip install numpy
+pip install pixiedust
+pip install -U matplotlib
+pip install -U scikit-learn
+pip install pandas
+pip install ipysheet
+pip install qgrid
+jupyter nbextension enable --py --sys-prefix qgrid
